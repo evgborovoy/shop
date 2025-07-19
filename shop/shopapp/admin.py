b47cb53f21a699b61dddf97f1c011dbb268aee0a
Evgeniy_Borovoy <borovoyevgen@gmail.com>
2025-07-19 22:55:34 +0400

    add Product and Order models in admin panel

--- shop/shopapp/admin.py
+++ shop/shopapp/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Product, Order
 
-# Register your models here.
+
+class OrderInline(admin.TabularInline):
+    model = Product.orders.through
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderInline
+    ]
+    list_display = ["pk", "name", "price", "description_short", "discount"]
+    list_display_links = ["pk", "name"]
+    ordering = ["-pk"]
+    search_fields = ["name", "description"]
+    fieldsets = [
+        (None, {
+            "fields": ("name", "description"),
+        }),
+        ("Price options", {
+            "fields": ("price", "discount"),
+        }),
+        ("Options", {
+            "fields": ("archived",),
+            "classes": ("collapse",),
+            "description": "Field 'archived' is for soft delete",
+        }),
+    ]
+
+
+class ProductInline(admin.TabularInline):
+    model = Order.products.through
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductInline,
+    ]
+    list_display = ["user_verbose", "created_at", "promocode", "delivery_address"]
+
+    def get_queryset(self, request):
+        return Order.objects.select_related("user").prefetch_related("products")
+
+    def user_verbose(self, obj: Order) -> str:
+        return f"{obj.user.first_name} {obj.user.last_name}" or f"{obj.user.first_name}" or obj.user.username
--- shop/shopapp/models.py
+++ shop/shopapp/models.py
@@ -13,6 +13,15 @@ class Product(models.Model):
     class Meta:
         ordering = ["name"]
 
+    @property
+    def description_short(self) -> str:
+        if len(self.description) < 50:
+            return self.description
+        return self.description[:50] + "..."
+
+    def __str__(self) -> str:
+        return f"Product(pk={self.pk}), name={self.name!r}"
+
 
 class Order(models.Model):
     delivery_address = models.TextField(null=True, blank=True)
