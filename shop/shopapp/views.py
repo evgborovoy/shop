65120c66aa405c356ff46079f300b627e052cc7e
Evgeniy_Borovoy <borovoyevgen@gmail.com>
2025-07-21 13:54:42 +0400

    create Product and Order forms

--- shop/shopapp/views.py
+++ shop/shopapp/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 from shopapp.models import Product, Order
+from shopapp.forms import ProductForm, OrderForm
 
 
 def shop_index(request: HttpRequest) -> HttpResponse:
@@ -21,3 +23,36 @@ def orders_list(request: HttpRequest) -> HttpResponse:
         "orders": Order.objects.select_related("user").prefetch_related("products").all()
     }
     return render(request, "shopapp/orders_list.html", context=context)
+
+
+def create_product(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            url = reverse("shopapp:products_list")
+            return redirect(url)
+    else:
+        form = ProductForm()
+    context = {
+        "form": form
+    }
+    return render(request, "shopapp/create_product.html", context=context)
+
+@login_required
+def create_order(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.user = request.user
+            order.save()
+            form.save_m2m()
+            url = reverse("shopapp:orders_list")
+            return redirect(url)
+    else:
+        form = OrderForm()
+    context = {
+        "form": form
+    }
+    return render(request, "shopapp/create_order.html", context=context)
